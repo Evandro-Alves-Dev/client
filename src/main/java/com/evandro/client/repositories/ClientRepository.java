2de43926acc06ee7e5c363afc8e85391b3ca4795
evandro.alves <evandroacer3@gmail.com>
2022-06-24 14:48:43 -0300

    Implement repository

---
+++ src/main/java/com/evandro/client/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.evandro.client.repositories;
+
+import com.evandro.client.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
